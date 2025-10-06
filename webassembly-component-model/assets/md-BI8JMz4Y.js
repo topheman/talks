1890d274466d50f55475cb14ce94a46462ca65a0
Christophe Rosset <tophe@topheman.com>
2025-10-06 18:18:51 +0200

    chore(wcm): build

---
+++ webassembly-component-model/assets/md-BI8JMz4Y.js
@@ -0,0 +1,10 @@
+import{b as m,o as a,w as r,g as e,i,v as p,x as l,C as o}from"./modules/vue-DEG6N64q.js";import{I as c}from"./slidev/center-geQvi2E1.js";import{u as h,f as d}from"./slidev/context-fpMMTjz8.js";import"./index-BUhPof8T.js";import"./modules/shiki-B8gquvyj.js";const u="/talks/webassembly-component-model/assets/qrcode-BmB906LC.png",w={__name:"slides.md__slidev_26",setup(b){const{$clicksContext:s,$frontmatter:n}=h();return s.setup(),(g,t)=>(a(),m(c,p(l(o(d)(o(n),25))),{default:r(()=>[...t[0]||(t[0]=[e("p",null,"Merci ! 🎉 Questions ?",-1),e("h1",null,"WebAssembly Component Model 🧩",-1),e("div",{class:"flex justify-center mb-10"},[i(` <QRCode
+    :width="400"
+    :height="400"
+    type="svg"
+    data="https://github.com/topheman/webassembly-component-model-experiments"
+    :margin="10"
+    :imageOptions="{ margin: 10 }"
+    :dotsOptions="{ color: '#6c63ff' }"
+    image="/WebAssembly_Logo.svg"
+/> `),e("img",{src:u,width:"200",height:"200"})],-1),e("h2",null,[e("a",{href:"https://github.com/topheman/webassembly-component-model-experiments",target:"_blank"},"topheman/webassembly-component-model-experiments")],-1),e("h3",null,[e("a",{href:"https://topheman.github.io/me/",target:"_blank"},"Christophe Rosset (topheman)")],-1)])]),_:1},16))}};export{w as default};
